4a9384f5329bef609ee206b57d6aa2d7d38482aa
Malcolm <137590820+HydrogenOS1@users.noreply.github.com>
2024-01-21 11:34:33 +1000

    HydrogenOS1-patch-1
    Tidy Up

--- ALand/src/main/java/com/malcolmjh/aland/commands/chatcommand.java
+++ ALand/src/main/java/com/malcolmjh/aland/commands/chatcommand.java
@@ -19,35 +19,32 @@ public class chatcommand implements CommandExecutor {
             Player p = (Player) sender;
             //Check if the command has arguments.
             if (args.length == 0) {
-                //If no args return the error
-                p.sendMessage(ChatColor.RED + "Unknown or incomplete command, see below for error");
-                p.sendMessage(ChatColor.GRAY + "chat" + ChatColor.RED + "<--[HERE]");
+                p.sendMessage(ChatColor.RED + "Incomplete command, see below for usage!");
                 p.sendMessage(ChatColor.DARK_GREEN + "Usage: /chat <message>");
-            }else {
-                //Make the "name" var Pt1
+            } else {
+                //Make the "message" var
                 StringBuilder builder = new StringBuilder();
-                for(int i = 0; i < args.length; i++) {
+                for (int i = 0; i < args.length; i++) {
                     builder.append(args[i]);
                     builder.append(" ");
-            }
-            //Make the "name" var Pt2
-            String name = builder.toString();
-            name = name.stripTrailing();
-            //Chat API call
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://api.malcolmjh.com:8080/check_name?name=" + name))
+                }
+                String message = builder.toString();
+                message = message.stripTrailing();
+                //Call the API
+                HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://api.malcolmjh.com:8080/check_name?name=" + message))
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
-            HttpResponse<String> response = null;
-            try {
-                response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException e) {
+                HttpResponse<String> response = null;
+                try {
+                    response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                } catch (IOException e) {
                     throw new RuntimeException(e);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            // Print the response
-            p.sendMessage(ChatColor.DARK_BLUE + response.body());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                // Print the response
+                p.sendMessage(ChatColor.DARK_BLUE + response.body());
             }
         }
         return true;
